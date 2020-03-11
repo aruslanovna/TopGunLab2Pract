2d38917c51dd7c9097e75c26c59fab4b6408c094
aruslanovna <a.khrystova@gmail.com>
2020-03-11 20:08:23 +0200

    Task4

--- TopGunLab2Pract/Program.cs
+++ TopGunLab2Pract/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using TopGunLab2Pract.ModelAndLinq;
+using TopGunLab2Pract.Task4;
 
 namespace TopGunLab2Pract
 {
@@ -10,21 +11,11 @@ namespace TopGunLab2Pract
         {
             static void Main(string[] args)
             {
+            int[] allNumbers = { 3, 6, 2, 5, 24, 63, 26, 32, 62, 7, 0, 6, 2 };
 
-            //WorkPlace wp1 = new WorkPlace() {Id = 1, city= City.Dnepr, Name=Name.Epam, NumberOfWorkers=100 };
-            //WorkPlace wp2 = new WorkPlace() { Id = 2, city = City.Dnepr, Name = Name.TeamDev, NumberOfWorkers = 100 };
-            //WorkPlace wp3 = new WorkPlace() { Id = 3, city = City.Kharkiv, Name = Name.TeamDev, NumberOfWorkers = 140 };
-            //WorkPlace wp4 = new WorkPlace() { Id = 4, city = City.Kyiv, Name =Name.Nix, NumberOfWorkers = 200 };
-            //WorkPlace wp5 = new WorkPlace() { Id = 5, city = City.Lviv, Name = Name.Nix, NumberOfWorkers = 100 };
-            //WorkPlace wp6 = new WorkPlace() { Id = 6, city = City.Odessa, Name = Name.Epam, NumberOfWorkers = 300 };
-
-            //Worker w1 = new Worker() { Id = 1, Name = "Igor", Experience = 20, Position = Position.Developer, Salary = 200, City = City.Odessa, CompanyName = CompanyName.Epam };
-            //Worker w2 = new Worker() { Id = 2, Name = "Oleg", Experience = 30, Position = Position.BusinessAnalit, Salary = 100, City = City.Kharkiv, CompanyName = CompanyName.TeamDev };
-            //Worker w3 = new Worker() { Id = 3, Name = "Pavel", Experience = 14, Position = Position.BusinessAnalit, Salary = 200, City = City.Kharkiv, CompanyName = CompanyName.TeamDev };
-            //Worker w4 = new Worker() { Id = 4, Name = "Mark", Experience = 30, Position = Position.Developer, Salary = 400, City = City.Kharkiv, CompanyName = CompanyName.Epam };
-            //Worker w5 = new Worker() { Id = 5, Name = "Mark", Experience = 40, Position = Position.Developer, Salary = 230, City = City.Lviv, CompanyName = CompanyName.Nix };
-            //Worker w6 = new Worker() { Id = 6, Name = "Sasha", Experience = 32, Position = Position.Developer, Salary = 280, City = City.Odessa, CompanyName = CompanyName.Nix };
-
+            EvenOdd e = new EvenOdd();
+            e.GetEven(allNumbers);
+            e.GetOdd(allNumbers);
 
             ProcessData pr = new ProcessData();
             pr.GetWorkersGroupByCompany();
---
+++ TopGunLab2Pract/Task4/EvenOdd.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace TopGunLab2Pract.Task4
+{
+  public  class EvenOdd
+    {
+        
+
+        public void GetEven(int[] allNum)
+        {
+            int[] evenArr = allNum
+                     .Where(item => item % 2 == 0)
+                     .Select(item => item)
+                     .ToArray();
+            string arrType="Even numbers: ";
+            PrintArr(evenArr, arrType);
+            
+        }
+        public void GetOdd(int[] allNum)
+        {
+            int[] oddArr = allNum
+                     .Where(item => item % 2 != 0)
+                     .Select(item => item)
+                     .ToArray();
+            string arrType ="Odd numbers: ";
+            PrintArr(oddArr, arrType);
+        }
+
+        public void PrintArr(int[] arr, string arrType)
+        {
+            Console.WriteLine(arrType);
+            foreach (var t in arr)
+            {
+                Console.Write(t+", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
